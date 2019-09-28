52ec86534e8df08e8dfb97f426fbb46d9e9d6d69
Jordi Armengol Estapé <me@jordiae.com>
2019-09-28 23:31:10 +0200

    Scraper MVP is working

---
+++ src/scraper/scraper.py
@@ -0,0 +1,112 @@
+import os, sys
+from datetime import datetime
+import ntpath
+import random
+random.seed(1)
+
+PGA_PATH = os.path.join('..', '..', 'bin', 'pga')
+SIVA_PATH = os.path.join('..', '..', 'bin', 'siva')
+ENRY_PATH = os.path.join('..', '..', 'bin', 'enry')
+CRAWLED_URLS_PATH = os.path.join('..', '..', 'data', 'crawled_urls')
+URLS_FILE = 'urls-2019-09-24-21:07:51.txt'
+SCRAPED_REPOS_PATH = os.path.join('..', '..', 'data', 'scraped_repos')
+CORPUS_PATH = os.path.join('..', '..', 'data', 'corpora')
+
+
+def write_corpus(corpus_files, corpus_path, corpus_name, stats, ndocs, ndevtest):
+    os.system('mkdir -p ' + corpus_path)
+    with open(os.path.join(corpus_path, 'stats.log'), 'w') as f:
+        f.writelines(stats)
+    random.shuffle(corpus_files)
+    train, dev, test = corpus_files[0:-ndevtest*2], corpus_files[-ndevtest*2:-ndevtest], corpus_files[-ndevtest:]
+    with open(os.path.join(corpus_path, 'train.txt'), 'a') as corpus:
+        for filepath in train:
+            filename, file_extension = os.path.splitext(ntpath.basename(filepath))
+            content = ' == ' + filename + ' ' + file_extension + ' ==\n'
+            print('Writing', filepath)
+            content += open(filepath, 'r').read()
+            content = ''.join([s for s in content.splitlines(True) if s.strip()])  # remove whitespace/tab only lines
+            content = os.linesep.join([s for s in content.splitlines() if s])  # remove empty lines
+            content += '\n\n'
+            corpus.write(content)
+    with open(os.path.join(corpus_path, 'valid.txt'), 'a') as corpus:
+        for filepath in dev:
+            filename, file_extension = os.path.splitext(ntpath.basename(filepath))
+            content = ' == ' + filename + ' ' + file_extension + ' ==\n'
+            print('Writing', filepath)
+            content += open(filepath, 'r').read()
+            content = ''.join([s for s in content.splitlines(True) if s.strip()])  # remove whitespace/tab only lines
+            content = os.linesep.join([s for s in content.splitlines() if s])  # remove empty lines
+            content += '\n\n'
+            corpus.write(content)
+    with open(os.path.join(corpus_path, 'test.txt'), 'a') as corpus:
+        for filepath in test:
+            filename, file_extension = os.path.splitext(ntpath.basename(filepath))
+            content = ' == ' + filename + ' ' + file_extension + ' ==\n'
+            print('Writing', filepath)
+            content += open(filepath, 'r').read()
+            content = ''.join([s for s in content.splitlines(True) if s.strip()])  # remove whitespace/tab only lines
+            content = os.linesep.join([s for s in content.splitlines() if s])  # remove empty lines
+            content += '\n\n'
+            corpus.write(content)
+
+
+def main():
+    enry = os.path.realpath(ENRY_PATH)
+    urls = open(os.path.join(CRAWLED_URLS_PATH, URLS_FILE), 'r').readlines()
+    corpus_path = os.path.realpath(os.path.join(CORPUS_PATH, URLS_FILE[:-4]))
+    try:
+        os.mkdir(SCRAPED_REPOS_PATH)
+    except BaseException as e:
+        print(str(e))
+    os.chdir(SCRAPED_REPOS_PATH)
+    try:
+        os.mkdir(os.path.join(SCRAPED_REPOS_PATH, URLS_FILE[:-4]))
+    except BaseException as e:
+        print(str(e))
+    os.chdir(os.path.join(SCRAPED_REPOS_PATH, URLS_FILE[:-4]))
+    for url in urls:
+        continue
+        os.system('git clone ' + url)
+        dir_name = []
+    corpus_files = []
+    stats = {}
+    for dir_name, subdir_list, file_list in os.walk('.'):
+        # print('Found directory: %s' % dir_name)
+        for fname in file_list:
+            # print('\t%s' % fname)
+            output_lines = os.popen(enry + ' ' + os.path.join(dir_name, fname)).readlines()
+            for line in output_lines:
+                if line.split()[0] == 'language:':
+                    language = None
+                    try:
+                        language = line.split()[1]
+                        if language == 'Pickle' or language == 'SVG' or language == 'Text':
+                            break
+                            #print(output_lines, dir_name)
+                            #input()
+                        #print('\t%s' % fname, 'is source code: ', language)
+                        corpus_files.append(os.path.realpath(os.path.join(dir_name, fname)))
+                        if language in stats:
+                            stats[language] += 1
+                        else:
+                            stats[language] = 1
+                    except BaseException as e:
+                        pass
+                        # print('\t%s' % fname, 'is NOT source code: ')
+                        # print(output_lines)
+                        # input()
+                    break
+    print(stats)
+    stats_lines = []
+    total = 0
+    for key, value in stats.items():
+        total += value
+    for key, value in stats.items():
+        stats_lines.append(key + ' ' + str(value) + ' ' + str(100*value/total)[:6] + '%\n')
+    print(len(corpus_files), corpus_files[50])
+    write_corpus(corpus_files=corpus_files, corpus_path=corpus_path, corpus_name=URLS_FILE[:-4], stats=stats_lines, ndocs=total, ndevtest=50)
+
+
+if __name__ == '__main__':
+    main()
